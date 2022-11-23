33629b9620b5fd67fcff1bf2f18cac93d599871b
Diego Ortiz Hurtado <73130246+museortizmedia@users.noreply.github.com>
2022-11-23 16:09:45 -0500

    Updates

---
+++ precache-manifest.ea892c6f41d12b46fa8fe9014b1e6a0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "20d9b1beb3480cc908a9",
+    "url": "/TuGuiaJamundi/static/css/main.cdda6799.chunk.css"
+  },
+  {
+    "revision": "20d9b1beb3480cc908a9",
+    "url": "/TuGuiaJamundi/static/js/main.20d9b1be.chunk.js"
+  },
+  {
+    "revision": "dd147fa618cfd7797da6",
+    "url": "/TuGuiaJamundi/static/css/1.940d3fdd.chunk.css"
+  },
+  {
+    "revision": "dd147fa618cfd7797da6",
+    "url": "/TuGuiaJamundi/static/js/1.dd147fa6.chunk.js"
+  },
+  {
+    "revision": "76f99c8c91df287ca70e",
+    "url": "/TuGuiaJamundi/static/js/runtime~main.76f99c8c.js"
+  },
+  {
+    "revision": "f06377b5dae2a50ec5938f70d03f4fe7",
+    "url": "/TuGuiaJamundi/static/media/Pick.f06377b5.png"
+  },
+  {
+    "revision": "21bb84996287ffdd3b95e416b3bc0d4e",
+    "url": "/TuGuiaJamundi/static/media/logo1080.21bb8499.png"
+  },
+  {
+    "revision": "3602503253ca177ba982c138285a0607",
+    "url": "/TuGuiaJamundi/index.html"
+  }
+];
